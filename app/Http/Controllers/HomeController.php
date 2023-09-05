3bd36cf978ca60c0f6fa4ff07baad5a3eb92c024
Guilherme Soares <guilherme.sousa009@hotmail.com>
2023-09-05 18:03:15 -0300

    Mostrando os dados

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Doctor;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
@@ -10,15 +11,21 @@ class HomeController extends Controller
 {
 
     public function index() {
-        return view('user.home');
+        if(Auth::id()) {
+            return redirect('home');
+        }else{
+            $doctor = Doctor::all();
+            return view('user.home', compact('doctor'));
+        }
     }
 
     public function redirect() {
-        
+        $doctor = Doctor::all();
         if(Auth::id()) {
 
+            
             if(Auth::user()->usertype == '0'){
-                return view('user.home');
+                return view('user.home', compact('doctor'));
             }else{
                 return view('admin.home');
             }
